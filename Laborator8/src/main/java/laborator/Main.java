620667400bd5ddb8811da3764fda28794162818e
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-04-22 12:14:24 +0300

    Update Main.java

--- Laborator8/src/main/java/laborator/Main.java
+++ Laborator8/src/main/java/laborator/Main.java
@@ -6,17 +6,18 @@ import laborator.dao.Movies;
 public class Main {
     public static void main(String[] args)
     {
-        Singleton.setUrl("jdbc:mysql://localhost:3307/db");
-        Singleton.setUser("root");
-        Singleton.setPassword("STUDENT");
-        Singleton.getConnection();
+        Singleton singleton = Singleton.getInstance();
+        singleton.setUrl("jdbc:mysql://localhost:3307/db");
+        singleton.setUser("root");
+        singleton.setPassword("STUDENT");
+        singleton.getConnection();
         Movies.init();
-        Movies.findByID(2,Singleton.getConnection());
+        Movies.findByID(2,singleton.getConnection());
         System.out.println();
         Genres.init();
-        Genres.findByID(1,Singleton.getConnection());
-        //Genres.insertNewRow("SF",Singleton.getConnection());
-        Movies.insertNewRow("Armageddon","1998-06-30",90,4,Singleton.getConnection());
+        Genres.findByID(1,singleton.getConnection());
+        //Genres.insertNewRow("SF",singleton.getConnection());
+        Movies.insertNewRow("Armageddon","1998-06-30",90,4,singleton.getConnection());
         Tools.insertRowsFromIMDbFile();
     }
 }
