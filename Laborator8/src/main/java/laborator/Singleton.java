6e02e729af7363b28057cc11b249d7123b60481b
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-04-22 12:13:29 +0300

    Update Singleton.java

--- Laborator8/src/main/java/laborator/Singleton.java
+++ Laborator8/src/main/java/laborator/Singleton.java
@@ -10,6 +10,7 @@ public class Singleton {
     private static String driver;
     private static String user;
     private static String password;
+    private static Singleton instance = null;
     private Singleton(){}
     public static Connection getConnection(){
         if(connection!=null){
@@ -22,6 +23,12 @@ public class Singleton {
             System.err.println("SQLException:" + e);
         }
         return connection;
+    }
+    public static Singleton getInstance()
+    {
+        if(instance == null)
+            instance = new Singleton();
+        return instance;
     }
     public static void setConnection(Connection con) {
         connection = con;
