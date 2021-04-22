fac2e9ecdc305f10071ec9d9f231fb99501d5bc5
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-04-22 13:53:33 +0300

    Delete Laborator5 directory

--- Laborator5/src/laborator/CatalogCommand.java
+++
@@ -1,7 +0,0 @@
-package laborator;
-
-import java.io.IOException;
-
-public abstract class CatalogCommand extends Catalog{
-    abstract void execute(Item ... items) throws InvalidCatalogException;
-}
--- Laborator5/src/laborator/InvalidCatalogException.java
+++
@@ -1,7 +0,0 @@
-package laborator;
-
-public class InvalidCatalogException extends Exception{
-    public InvalidCatalogException(String ex) {
-        super(ex);
-    }
-}
