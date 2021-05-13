ac2cdcb2f1db9762df11adb3bf6c168216e39d2d
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-05-13 16:08:32 +0300

    initial compulsory

---
+++ Laborator11(compulsory)/laborator/src/test/java/com/laborator/demo/Laborator11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laborator.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laborator11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
